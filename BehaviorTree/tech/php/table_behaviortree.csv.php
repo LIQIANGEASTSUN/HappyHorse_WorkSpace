8ae27d2c5a2ea8c955cdbd523cc47b4fadd71541
LQ <liqiangeastsun@163.com>
2023-01-12 15:07:50 +0800

    modify

--- BehaviorTree/tech/php/table_behaviortree.csv.php
+++ BehaviorTree/tech/php/table_behaviortree.csv.php
@@ -3,5 +3,12 @@ return array(
 1=>["ID"=>1,"EnName"=>"DoFunction","CnName"=>"功能/执行功能","Type"=>2,"FloatValue"=>0,"IntValue"=>0,"BoolValue"=>0,"StringValue"=>""],
 2=>["ID"=>2,"EnName"=>"InHooking","CnName"=>"家园宠物/是否在挂机中","Type"=>5,"FloatValue"=>0,"IntValue"=>0,"BoolValue"=>0,"StringValue"=>""],
 3=>["ID"=>3,"EnName"=>"InHookPlace","CnName"=>"家园宠物/在挂机建筑位置","Type"=>5,"FloatValue"=>0,"IntValue"=>0,"BoolValue"=>0,"StringValue"=>""],
+4=>["ID"=>4,"EnName"=>"DistancePlayer","CnName"=>"宠物/与玩家距离","Type"=>0,"FloatValue"=>0,"IntValue"=>0,"BoolValue"=>0,"StringValue"=>""],
+5=>["ID"=>5,"EnName"=>"ArriveFollowPos","CnName"=>"宠物/到达跟随点","Type"=>5,"FloatValue"=>0,"IntValue"=>0,"BoolValue"=>0,"StringValue"=>""],
+6=>["ID"=>6,"EnName"=>"HasTarget","CnName"=>"宠物/有攻击目标","Type"=>5,"FloatValue"=>0,"IntValue"=>0,"BoolValue"=>0,"StringValue"=>""],
+7=>["ID"=>7,"EnName"=>"ValidSkill","CnName"=>"宠物/技能有效","Type"=>5,"FloatValue"=>0,"IntValue"=>0,"BoolValue"=>0,"StringValue"=>""],
+8=>["ID"=>8,"EnName"=>"TargetInAttackDistance","CnName"=>"宠物/目标在攻击范围内","Type"=>5,"FloatValue"=>0,"IntValue"=>0,"BoolValue"=>0,"StringValue"=>""],
+9=>["ID"=>9,"EnName"=>"PetHp","CnName"=>"宠物/血量","Type"=>0,"FloatValue"=>0,"IntValue"=>0,"BoolValue"=>0,"StringValue"=>""],
+10=>["ID"=>10,"EnName"=>"ValidPath","CnName"=>"宠物/路径有效","Type"=>0,"FloatValue"=>0,"IntValue"=>0,"BoolValue"=>0,"StringValue"=>""],
 );
 ?>
